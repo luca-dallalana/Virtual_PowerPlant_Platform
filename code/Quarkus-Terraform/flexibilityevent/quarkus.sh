afc4824480a00fc6beaf8d9a35c8d40bb128448a
Diogo Rodrigues <diogo.teixeira.rodrigues@tecnico.ulisboa.pt>
2026-05-10 16:45:51 +0100

    Fix: Update port mappings from 8085 to 8084 in Dockerfiles, docker-compose, and application properties of FlexibilityEvent

--- code/Quarkus-Terraform/flexibilityevent/quarkus.sh
+++ code/Quarkus-Terraform/flexibilityevent/quarkus.sh
@@ -11,4 +11,4 @@ sudo service docker start
 echo "Finished."
 sudo docker login -u "slguerreiro" -p "dfjfkd8Flnciw7"
 sudo docker pull slguerreiro/flexibilityevent:1.0.0-SNAPSHOT
-sudo docker run -d --name flexibilityevent -p 8085:8085 slguerreiro/flexibilityevent:1.0.0-SNAPSHOT
+sudo docker run -d --name flexibilityevent -p 8084:8084 slguerreiro/flexibilityevent:1.0.0-SNAPSHOT
