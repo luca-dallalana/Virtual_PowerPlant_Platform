3f82ece882dd1ce6f7bf9e5b243ee7dc00f3611b
Luca Dallalana <luca.dallalana@tecnico.ulisboa.pt>
2026-05-28 00:56:55 +0100

    Fix: Implemented Kafka Cluster replication

--- code/Kafka/EC2ChangeKafkaConfiguration.tf
+++ code/Kafka/EC2ChangeKafkaConfiguration.tf
@@ -10,39 +10,92 @@ terraform {
 }
 
 provider "aws" {
-  region      = "us-east-1"
-  }
+  region = "us-east-1"
+}
+
+variable "nBroker" {
+  description = "Number of Kafka brokers in the cluster"
+  type        = number
+  default     = 3
+}
 
 resource "aws_instance" "exampleKafkaConfiguration" {
-  ami                     = "ami-07ff62358b87c7116"
-  instance_type           = "t3.small"
-  count                   = 1
-  vpc_security_group_ids  = [aws_security_group.instance.id]
-  key_name                = "vockey"
+  ami                    = "ami-07ff62358b87c7116"
+  instance_type          = "t3.small"
+  count                  = var.nBroker
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  key_name               = "vockey"
 
-  user_data = "${file("creation.sh")}"
+  user_data = base64encode(templatefile("creation.sh", {
+    idBroker     = count.index
+    totalBrokers = var.nBroker
+  }))
 
   user_data_replace_on_change = true
-  
+
   tags = {
-    Name = "terraform-kafka"
+    Name = "terraform-kafka.${count.index}"
+  }
+}
+
+output "publicdnslist" {
+  value = formatlist("%v", aws_instance.exampleKafkaConfiguration.*.public_dns)
+}
+
+locals {
+  replica_directorys = [
+    "r1eMpKZRROex80kgn4_2-g",
+    "bWIe6tPFS3mKvq-W-_kgtQ",
+    "Z57HkCAASdifFa-QWCdGWA"
+  ]
+  quorum_voters = join(",", [
+    for i, instance in aws_instance.exampleKafkaConfiguration :
+    "${i + 1}@${instance.public_dns}:9093:${local.replica_directorys[i]}"
+  ])
+}
+
+resource "null_resource" "update_dns" {
+  count = var.nBroker
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("key.pem")
+    host        = aws_instance.exampleKafkaConfiguration[count.index].public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo -n '${join(":9093,", aws_instance.exampleKafkaConfiguration.*.public_dns)}:9093' > cluster_hosts",
+      "echo -n '${local.quorum_voters}' > cluster_hosts_old_format"
+    ]
   }
 }
 
 resource "aws_security_group" "instance" {
   name = var.security_group_name
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    ingress {
+
+  ingress {
     from_port   = 9092
     to_port     = 9092
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 9093
+    to_port     = 9093
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -57,4 +110,3 @@ variable "security_group_name" {
   type        = string
   default     = "terraform-example-instance2026"
 }
-
--- code/Kafka/creation.sh
+++ code/Kafka/creation.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 echo "Starting..."
 cd
-sudo yum -y install java-17-amazon-corretto-devel.x86_64  
+sudo yum -y install java-17-amazon-corretto-devel.x86_64
 sudo wget https://dlcdn.apache.org/kafka/4.1.1/kafka_2.13-4.1.1.tgz
 sudo tar -zxf kafka_2.13-4.1.1.tgz
 cd kafka_2.13-4.1.1
-KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)"
-sudo bin/kafka-storage.sh format --standalone -t $KAFKA_CLUSTER_ID -c config/server.properties
 
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 dnsname=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-hostname`
+clusterlist=$(cat /home/ec2-user/cluster_hosts)
+oldclusterlist=$(cat /home/ec2-user/cluster_hosts_old_format)
 
-sudo sed -i "s/listeners=PLAINTEXT:\/\/:9092,CONTROLLER:\/\/:9093/listeners=PLAINTEXT:\/\/0.0.0.0:9092,CONTROLLER:\/\/:9093/g" config/server.properties
-sudo sed -i "s/advertised.listeners=PLAINTEXT:\/\/localhost:9092,CONTROLLER:\/\/localhost:9093/advertised.listeners=PLAINTEXT:\/\/$dnsname:9092,CONTROLLER:\/\/localhost:9093/g" config/server.properties
+sudo sed -i "s/node.id=1/node.id=${idBroker + 1}/g" config/server.properties
+sudo sed -i "s/listeners=PLAINTEXT:\/\/:9092,CONTROLLER:\/\/:9093/listeners=PLAINTEXT:\/\/0.0.0.0:9092,CONTROLLER:\/\/0.0.0.0:9093/g" config/server.properties
+sudo sed -i "s/advertised.listeners=PLAINTEXT:\/\/localhost:9092,CONTROLLER:\/\/localhost:9093/advertised.listeners=PLAINTEXT:\/\/$dnsname:9092/g" config/server.properties
+sudo sed -i "s/controller.quorum.bootstrap.servers=localhost:9093/controller.quorum.bootstrap.servers=$clusterlist/g" config/server.properties
 
-sudo bin/kafka-server-start.sh config/server.properties&
+KAFKA_CLUSTER_ID=MkU3OEVBNTcwNTJENDM2M
+sudo bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config/server.properties --initial-controllers $oldclusterlist
 
-# Create known application topics if missing.
-sleep 5
+echo "sudo bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config/server.properties --initial-controllers $oldclusterlist"
+
+sudo bin/kafka-server-start.sh config/server.properties &
+
+%{ if idBroker == 0 }
+sleep 15
 topics=(
-	"Energy-Discharged-By-Zone"
-	"Generated-Energy-By-Prosumer"
-	"Consumed-Energy-By-Prosumer"
-	"Average-SoC"
-	"GridBalancingRecommendation"
-	"flexibility-offers"
+  "Flexibility-Offers"
+  "GridBalancingRecommendation"
+  "Energy-Discharged-Zone"
+  "Energy-Generated-Prosumer"
+  "Energy-Consumed-Prosumer"
+  "Average-SoC"
 )
-
-for topic in "${topics[@]}"; do
-	sudo bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists \
-		--topic "$topic" --partitions 1 --replication-factor 1
+for topic in "$${topics[@]}"; do
+  sudo bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists \
+    --topic "$topic" --partitions 3 --replication-factor 3
 done
+%{ endif }
 
 echo "Finished."
